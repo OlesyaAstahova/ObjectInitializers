83008ddf8f6a17f80e77751b31db29ee75123b6d
Олесуа и Юля <astahova.98@gmail.com>
2018-02-05 09:44:07 +0700

    Класс Point и создание объектов Point

---
+++ ObjectInitializers/ObjectInitializers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Object Init Syntex *****\n");
+
+            Point firstPoint = new Point();
+            firstPoint.X = 10;
+            firstPoint.Y = 10;
+            firstPoint.DisplayStats();
+
+            Point anotherPoint = new Point(20, 20);
+            anotherPoint.DisplayStats();
+
+            Point finalPoint = new Point { X = 30, Y = 30 };
+            finalPoint.DisplayStats();
+            Console.ReadLine();
+        }
+    }
+}
